d2372b7c9abdcffeb0b5a406ff089a07de6a8762
longy1 <longy8@mail2.sysu.edu.cn>
2021-11-29 16:41:04 +0800

    Feature: implement 8.4

---
+++ 08_Goroutines_and_Channels/11_pipeline1.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	naturals := make(chan int)
+	squares := make(chan int)
+
+	go func() {
+		for x := 0; x < 100; x++ {
+			naturals <- x
+		}
+		close(naturals)
+	}()
+
+	go func() {
+		for x, ok := <-naturals; ok; x, ok = <-naturals {
+			squares <- x * x
+		}
+		close(squares)
+	}()
+
+	for {
+		x, ok := <-squares
+		if !ok {
+			break
+		}
+		fmt.Println(x)
+	}
+}
