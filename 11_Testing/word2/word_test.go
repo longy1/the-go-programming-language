398e1c957db8d00dc5411e5df6b11a7e50a1d09b
longy1 <longy8@mail2.sysu.edu.cn>
2021-12-10 15:28:52 +0800

    Feature: implement section 11.3

--- 11_Testing/word2/word_test.go
+++ 11_Testing/word2/word_test.go
@@ -1,6 +1,10 @@
 package word
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
 
 func TestIsPalindrome(t *testing.T) {
 	tests := []struct {
@@ -22,10 +26,61 @@ func TestIsPalindrome(t *testing.T) {
 		{"desserts", false},   // semi-palindrome
 		{"中国中", true},
 		{"中过", false},
+		{"șȘ", true},
 	}
 	for _, test := range tests {
 		if got := IsPalindrome(test.input); got != test.want {
 			t.Errorf(`IsPalindrome(%q) = %v`, test.input, got)
+		}
+	}
+}
+
+func randomPalindrome(rng *rand.Rand) string {
+	length := rng.Intn(25) // [0, 25)
+	runes := make([]rune, length)
+	for i := 0; i < length; i++ {
+		r := rune(rng.Intn(1000)) // \u0000 ~ \u0999
+		runes[i] = r
+		runes[length-i-1] = r
+	}
+	return string(runes)
+}
+
+func nonRandomPalindrome(rng *rand.Rand) string {
+	length := rng.Intn(23) + 2 // [2, 25)
+	runes := make([]rune, length)
+	for i := 0; i < length; i++ {
+		r := rune(rng.Intn(1000)) // \u0000 ~ \u0999
+		runes[i] = r
+		runes[length-i-1] = r
+	}
+	randOffset := rand.Intn(25) + 1
+	randIdx := rand.Intn(length / 2)
+	runes[randIdx] = rune('a' + randOffset)
+	runes[length-1-randIdx] = rune('A' + randOffset - 1)
+	return string(runes)
+}
+
+func TestRandomPalindrome(t *testing.T) {
+	seed := time.Now().UTC().UnixNano()
+	t.Logf("Random seed: %d", seed)
+	rng := rand.New(rand.NewSource(seed))
+	for i := 0; i < 1000; i++ {
+		s := randomPalindrome(rng)
+		if !IsPalindrome(s) {
+			t.Errorf(`IsPalindrome(%q) = false`, s)
+		}
+	}
+}
+
+func TestNonRandomPalindrome(t *testing.T) {
+	seed := time.Now().UTC().UnixNano()
+	t.Logf("Random seed: %d", seed)
+	rng := rand.New(rand.NewSource(seed))
+	for i := 0; i < 1000; i++ {
+		s := nonRandomPalindrome(rng)
+		if IsPalindrome(s) {
+			t.Errorf(`IsPalindrome(%q) = true`, s)
 		}
 	}
 }
